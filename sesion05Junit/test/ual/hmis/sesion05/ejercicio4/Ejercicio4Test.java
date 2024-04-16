d8b9e03a5e9258bab50e0f2c09f1118ff6668bb7
Daniel Salas <dsa069@inlumine.ual.es>
2024-04-16 13:54:56 +0200

    sq	ewrt

--- sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio4Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio4Test.java
@@ -5,18 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class Ejercicio4Test {
-		@CsvSource({
-			"[1, 3, 5, 7, 9],[2, 4, 6, 8, 10],[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
-
-		})
-
-		@ParameterizedTest(name = "{index} => al1={0}, al2={1}, resultado={2}")
-		void testpaswor(List<Integer> al1, List<Integer> al2, List<Integer> result) {
+		@CsvFileSource(resources = "Ejercicio4.csv", delimiter=':', numLinesToSkip = 1)
+		@ParameterizedTest(name = "{index} => al1=({0}), al2=({1}), resultado=({2})")
+		@MethodSource
+		void testpaswor(List<String> al1, List<String> al2, List<String> result) {
 			// Arrange
-			MezclaLineal<Integer> e4 = new MezclaLineal<>();
+			MezclaLineal<String> e4 = new MezclaLineal<>();
 			// Act
 			// Assert
 			assertEquals(result, e4.LinealShake(al1,al2));
--- sesion05Junit/test/ual/hmis/sesion05/ejercicio5/Ejercicio5Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio5/Ejercicio5Test.java
@@ -1,5 +1,29 @@
 package ual.hmis.sesion05.ejercicio5;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 public class Ejercicio5Test {
+		@CsvSource({
+			"'',' '",
+			"pass,password demasiado corto",
+			"passo,********",
+			"sainbrai,********",
+			"sainbrais,Error",
+	 		"sainbrailake,************",
+			"sainbrailakedaimonsainbrailakedaimonsain,************",
+			"sainbrailakedaimonsainbrailakedaimonsainb,password demasiado largo"
+		})
+		
+		@ParameterizedTest(name = "{index} => Con password ({0}) sale {1}")
+		void testpaswor(String password, String result) {
+			// Arrange
+			Ejercicio5 e3 = new Ejercicio5();
+			// Act
+			// Assert
+			//assertEquals(result, e3.paswor(password));
+		}
+		
+	}
 
-}
+
