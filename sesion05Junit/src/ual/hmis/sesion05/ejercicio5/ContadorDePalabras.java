8e1b849bec2e78ac61da7524a7b4e9f94940a443
crv520-1 <crv520@inlumine.ual.es>
2024-04-21 14:44:17 +0200

    Avances interesantes

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio5/ContadorDePalabras.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio5/ContadorDePalabras.java
@@ -1,23 +1,38 @@
 package ual.hmis.sesion05.ejercicio5;
 
+import java.util.ArrayList;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class ContadorDePalabras {
 
-	private TreeMap<String, Integer> datos;
+	private static TreeMap<String, Integer> datos = new TreeMap<>();
+
+	public TreeMap<String, Integer> contadorPalabras (String[] palabras){
+		for (String palabra : palabras) {
+			datos.put(palabra, datos.containsKey(palabra) ? datos.get(palabra) + 1 : 1);
+		}
+		return datos;
+	}
 	
-	public TreeMap<String, Integer> contarPalabras (String palabras, int contador){
-		TreeMap<String, Integer> result = new TreeMap<>();
-		if (result.containsKey(palabras)) {
-			for (Entry<String, Integer> entry : result.entrySet()) {
-				contador = entry.getValue() + 1;
-				result.put(palabras, contador);
-			}
-		}else {
-			result.put(palabras, contador=1);
+	public TreeMap<Integer, String> ordenarFrecuencia (TreeMap<String, Integer> auxiliar){
+		TreeMap<Integer, String> result = new TreeMap<>();
+		for (Entry<String, Integer> entry : datos.entrySet()) {
+			result.put(entry.getValue(), entry.getKey());
 		}
 		return result;
 	}
-	
+
+	public static void main(String[] args) {
+		ContadorDePalabras cont = new ContadorDePalabras();
+		String[] palabras = {"Sofia", "Viator", "Marcelo", "Marcela", "Macarena", "Macarena"};
+		cont.contadorPalabras(palabras);
+		System.out.println(datos);
+		cont.contadorPalabras(palabras);
+		System.out.println(datos);
+		String[] palabras1 = {"Pedro"};
+		cont.contadorPalabras(palabras1);
+		System.out.println(datos);
+	}
+
 }
