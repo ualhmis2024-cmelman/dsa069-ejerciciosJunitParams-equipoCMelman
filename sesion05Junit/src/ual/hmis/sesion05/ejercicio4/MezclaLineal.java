46b3b3d9c23d218da5f55c9d15fee65776af0a6c
Daniel Salas <dsa069@inlumine.ual.es>
2024-06-10 13:07:38 +0200

    manejo de dupes

--- sesion05Junit/src/ual/hmis/sesion05/ejercicio4/MezclaLineal.java
+++ sesion05Junit/src/ual/hmis/sesion05/ejercicio4/MezclaLineal.java
@@ -3,32 +3,49 @@ import java.util.*;
 
 public class MezclaLineal<T extends Comparable<T>> {
 
-	public static <T extends Comparable<T>> T[] mezclar(T[] conjunto1, T[] conjunto2) {
-		// Método que mezcla dos arrays ordenados en uno solo ordenado
-			int i = 0, j = 0, k = 0;
-			T[] resultado = Arrays.copyOf(conjunto1, conjunto1.length + conjunto2.length);
+	public static <T extends Comparable<T>> T[] mezclar(T[] conjunto1, T[] conjunto2) {  int i = 0, j = 0, k = 0;
+    T[] resultado = Arrays.copyOf(conjunto1, conjunto1.length + conjunto2.length);
 
-			while (i < conjunto1.length && j < conjunto2.length) {
-				if (conjunto1[i].compareTo(conjunto2[j]) <= 0) {
-					resultado[k++] = conjunto1[i++];
-				} else {
-					resultado[k++] = conjunto2[j++];
-				}
-			}
+    while (i < conjunto1.length && j < conjunto2.length) {
+        int cmp = conjunto1[i].compareTo(conjunto2[j]);
+        if (cmp < 0) {
+            if (k == 0 || !resultado[k-1].equals(conjunto1[i])) {
+                resultado[k++] = conjunto1[i];
+            }
+            i++;
+        } else if (cmp > 0) {
+            if (k == 0 || !resultado[k-1].equals(conjunto2[j])) {
+                resultado[k++] = conjunto2[j];
+            }
+            j++;
+        } else {
+            if (k == 0 || !resultado[k-1].equals(conjunto1[i])) {
+                resultado[k++] = conjunto1[i];
+            }
+            i++;
+            j++;
+        }
+    }
 
-			// Copiar los elementos restantes de conjunto1, si hay alguno
-			while (i < conjunto1.length) {
-				resultado[k++] = conjunto1[i++];
-			}
+    // Copiar los elementos restantes de conjunto1, si hay alguno
+    while (i < conjunto1.length) {
+        if (k == 0 || !resultado[k-1].equals(conjunto1[i])) {
+            resultado[k++] = conjunto1[i];
+        }
+        i++;
+    }
 
-			// Copiar los elementos restantes de conjunto2, si hay alguno
-			while (j < conjunto2.length) {
-				resultado[k++] = conjunto2[j++];
-			}
+    // Copiar los elementos restantes de conjunto2, si hay alguno
+    while (j < conjunto2.length) {
+        if (k == 0 || !resultado[k-1].equals(conjunto2[j])) {
+            resultado[k++] = conjunto2[j];
+        }
+        j++;
+    }
 
-			return resultado;
-		}
-	}
+    return Arrays.copyOf(resultado, k);
+    }
+}
 	//	public static void main(String[] args) {
 	//		MezclaLineal<String> mezclador = new MezclaLineal<>();
 	//
